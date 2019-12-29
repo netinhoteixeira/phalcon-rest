0be9d798ccf4b35b7bba460793547f08c5f2aced
Francisco Ernesto Teixeira <fco.ernesto@gmail.com>
2019-12-29 04:52:20 -0300

    Fix detecting token value

--- src/PhalconRest/Middleware/AuthorizationMiddleware.php
+++ src/PhalconRest/Middleware/AuthorizationMiddleware.php
@@ -2,6 +2,7 @@
 
 namespace PhalconRest\Middleware;
 
+use PhalconApi\Constants\Services;
 use Phalcon\Events\Event;
 use Phalcon\Mvc\Micro;
 use Phalcon\Mvc\Micro\MiddlewareInterface;
@@ -26,6 +27,27 @@ class AuthorizationMiddleware extends Plugin implements MiddlewareInterface
         $token = $this->request->getToken();
         if ($token) {
             $this->authManager->authenticateToken($token);
+        } else {
+            // DONE: 2019-12-29 04:51 Francisco - Sometimes the value is not right fetched
+            $authHeader = $this->request->getHeader('AUTHORIZATION');
+            $authQuery = $this->request->getQuery('token');
+
+            if ((is_null($authHeader)) || (empty($authHeader))) {
+                $di = FactoryDefault::getDefault();
+                $request = $di->get(Services::REQUEST);
+
+                $headers = $request->getHeaders();
+
+                if (array_key_exists('Authorization', $headers)) {
+                    $authHeader = $headers['Authorization'];
+                }
+            }
+
+            $token = $authQuery ? $authQuery : $this->parseBearerValue($authHeader);
+
+            if ($token) {
+                $this->authManager->authenticateToken($token);
+            }
         }
 
         $allowed = $this->acl->isAllowed($this->userService->getRole(), $collection->getIdentifier(),
